83680034f777ede005241a8da0204250e7742625
Troykiri <V-Troitsky@yandex.ru>
2021-04-02 09:39:34 +0300

    fixed bugs

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,18 @@ function toogleMenu() {
   dropDownMenu.classList.toggle('drop-down-menu_active');
 
   if (dropDownMenu.classList.contains('drop-down-menu_active')) {
-    arrow.src = '/coolicon-up.svg';
+    arrow.src = '/CrestWave/coolicon-up.svg';
     headerNavElMenu.setAttribute('style', 'border-bottom: 1px solid #080E2C')
   } else {
-    arrow.src = '/coolicon-down.svg';
+    arrow.src = '/CrestWave/coolicon-down.svg';
+    headerNavElMenu.removeAttribute('style', 'border-bottom: 1px solid #080E2C')
+  }
+}
+
+function closeMenu(event) {
+  if (event.target.classList.contains('drop-down-menu__shadow')) {
+    dropDownMenu.classList.remove('drop-down-menu_active');
+    arrow.src = '/CrestWave/coolicon-down.svg';
     headerNavElMenu.removeAttribute('style', 'border-bottom: 1px solid #080E2C')
   }
 }
@@ -21,12 +29,14 @@ function toggleBurger() {
   burger.classList.toggle('burger_active');
 
   if (burger.classList.contains('burger_active')) {
-    headerBurger.src = '/close.svg'
+    headerBurger.src = '/CrestWave/close.svg'
   } else {
-    headerBurger.src = '/burger.svg'
+    headerBurger.src = '/CrestWave/burger.svg'
   }
 }
 
 headerNavElMenu.addEventListener('click', toogleMenu);
 
 headerBurger.addEventListener('click', toggleBurger);
+
+document.body.addEventListener('click', closeMenu)
